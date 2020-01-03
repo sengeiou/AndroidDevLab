c7f71dce08fa04b63fa43ea3926b3b5026f1fa11
yunjaepark <dbswoqkr123@gmail.com>
2020-01-03 14:16:56 +0900

    2020.01.03

--- app/src/main/java/com/example/technote/Layout/ConstraintLayout_Test.java
+++ app/src/main/java/com/example/technote/Layout/ConstraintLayout_Test.java
@@ -1,8 +1,13 @@
 package com.example.technote.Layout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.technote.R;
 
@@ -11,5 +16,13 @@ public class ConstraintLayout_Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_constraint_test);
+
+        Button goAnalogClock = (Button)findViewById(R.id.goAnalogClockExam);
+        goAnalogClock.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ConstraintLayout_Analog_Test.class));
+            }
+        });
     }
 }
