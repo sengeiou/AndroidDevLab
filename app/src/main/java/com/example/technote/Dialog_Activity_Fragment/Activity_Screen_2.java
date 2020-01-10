2f70f9d178410ddcf6a54b1582439da2d7a199dc
yunjaepark <dbswoqkr123@gmail.com>
2020-01-10 15:09:30 +0900

    20.1.10

--- app/src/main/java/com/example/technote/Dialog_Activity_Fragment/Activity_Screen_2.java
+++ app/src/main/java/com/example/technote/Dialog_Activity_Fragment/Activity_Screen_2.java
@@ -8,18 +8,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.technote.R;
 
+import at.grabner.circleprogress.CircleProgressView;
+
 public class Activity_Screen_2 extends AppCompatActivity {
     Toolbar screen_2_toolbar;
+    CircleProgressView mCircleView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_2);
 
-        screen_2_toolbar = (Toolbar)findViewById(R.id.screen_2_toolbar);
+        screen_2_toolbar = (Toolbar) findViewById(R.id.screen_2_toolbar);
         setSupportActionBar(screen_2_toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); // 타이틀 삭제
-    }
+        mCircleView = (CircleProgressView) findViewById(R.id.circleView);
+        mCircleView.setUnitVisible(false);
 
+
+    }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_screen_2, menu);
         return true;
