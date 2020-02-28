a048d9c62d69f3682385b93cc3be464c585e8df5
yunjaepark <dbswoqkr123@gmail.com>
2020-02-28 11:55:12 +0900

    2020.2.28 Retrofit 코드 수정

--- app/src/main/java/com/example/technote/Network/RetrofitExample/RetrofitExample.java
+++ app/src/main/java/com/example/technote/Network/RetrofitExample/RetrofitExample.java
@@ -6,22 +6,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Response;
 import com.example.technote.R;
-import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Response;
 
 public class RetrofitExample extends AppCompatActivity {
-    private Retrofit mRetrofit;
-    private GitHubService mRetrofitAPI;
-    private Call<Repo> mCallMovieList;
-    private Gson mGson;
     private TextView textView;
 
     private static final String url =
@@ -32,34 +26,21 @@ public class RetrofitExample extends AppCompatActivity {
         setContentView(R.layout.activity_network_retrofit_example);
 
         textView = (TextView)findViewById(R.id.retrofit_text);
-        setRetrofitInit();
-//        callMovieList();
-    }
 
-    private void setRetrofitInit() {
-        mRetrofit = new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        mRetrofitAPI = mRetrofit.create(GitHubService.class);
-        final Call<List<Repo>> repos = mRetrofitAPI.listRepos("sarang628");
-        repos.enqueue(mRetrofitCallback);
-    }
+        Call<ArrayList<JsonObject>> res = NetRetrofit.getInstance().getService().getListRepos("YunJaePark3908");
+        res.enqueue(new Callback<ArrayList<JsonObject>>() {
+            @Override
+            public void onResponse(Call<ArrayList<JsonObject>> call, Response<ArrayList<JsonObject>> response) {
+                Log.d("RequestResult","RetrofitExample, Type : get, Result : onResponse");
+                if (response.body() != null)
+                    textView.setText(response.body().toString());
+            }
 
+            @Override
+            public void onFailure(Call<ArrayList<JsonObject>> call, Throwable t) {
+                Log.d("RequestResult","RetrofitExample, Type : get, Result : onFailure, Error Message : " + t.getMessage());
+                Log.e("Err", t.getMessage());
+            }
+        }); }
 
-    private Callback<List<Repo>> mRetrofitCallback = new Callback<List<Repo>>() {
-
-        @Override
-        public void onResponse(Call<List<Repo>> call, retrofit2.Response<List<Repo>> response) {
-            Log.d("onResponse","RetrofitExample, result: onResponse");
-            textView.setText(response.toString());
-            //MovieListVO mMovieListVO = (MovieListVO) mGson.fromJson(result, MovieListVO.class);
-        }
-
-        @Override
-        public void onFailure(Call<List<Repo>> call, Throwable t) {
-            t.printStackTrace();
-            Log.d("onResponse","RetrofitExample, result: onFailure");
-        }
-    };
 }
